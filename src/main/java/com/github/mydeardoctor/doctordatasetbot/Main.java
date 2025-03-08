1a1cd3bbb4578283d33c4a255f2e09899d131370
my_dear_doctor <mydeardoctor10@gmail.com>
2025-03-09 00:39:28 +0500

    Настроил логгер

--- src/main/java/com/github/mydeardoctor/doctordatasetbot/Main.java
+++ src/main/java/com/github/mydeardoctor/doctordatasetbot/Main.java
@@ -1,6 +1,9 @@
 package com.github.mydeardoctor.doctordatasetbot;
 
+import ch.qos.logback.classic.LoggerContext;
 import com.github.mydeardoctor.doctordatasetbot.telegrambot.TelegramBot;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
@@ -9,6 +12,11 @@ public class Main
     public static void main(String[] args)
     {
         //TODO log thread
+        final Logger logger = LoggerFactory.getLogger(Main.class);
+        if(logger.isWarnEnabled())
+        {
+            logger.warn("Started main");
+        }
 
         try(final TelegramBotsLongPollingApplication telegramBotApplication =
                 new TelegramBotsLongPollingApplication())
@@ -25,6 +33,10 @@ public class Main
         {
             //TODO add logger. настройки логгера
             //TODO graceful close
+            LoggerContext loggerContext = (LoggerContext)LoggerFactory.getILoggerFactory();
+            loggerContext.stop();
+            //TODO read shutdown hooks
+
             e.printStackTrace();
         }
     }
