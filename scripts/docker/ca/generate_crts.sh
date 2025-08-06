1ed461e480d855b4af779f4f161c4f90c89c3c74
mydeardoctor <mydeardoctor10@gmail.com>
2025-08-06 19:18:41 +0500

    docker generate database admin csr

--- scripts/docker/ca/generate_crts.sh
+++ scripts/docker/ca/generate_crts.sh
@@ -3,7 +3,7 @@
 bash ./generate_ca_crt.sh
 
 bash ./generate_database_server_csr.sh
-#bash ./generate_database_admin_csr.sh
+bash ./generate_database_admin_csr.sh
 #bash ./generate_app_csr.sh
 #
 #bash ./sign_database_servers_csr.sh
--- scripts/docker/ca/generate_database_admin_csr.sh
+++ scripts/docker/ca/generate_database_admin_csr.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-if [[ ( -z "${POSTGRESQL_MAJOR_VERSION}" ) || \
-      ( -z "${DATABASE_ADMIN_USER}" ) || \
-      ( -z "${DATABASE_ADMIN_GROUP}" ) || \
+if [[ ( -z "${DATABASE_ADMIN_USER}" ) || \
+      ( -z "${DATABASE_ADMIN_UID}" ) || \
+      ( -z "${DATABASE_ADMIN_GID}" ) || \
+      ( -z "${DATABASE_CERTS_DIRECTORY}" ) || \
       ( -z "${DATABASE_ADMIN_KEY}" ) || \
       ( -z "${DATABASE_ADMIN_KEY_PERMISSIONS}" ) || \
       ( -z "${DATABASE_ADMIN_KEY_PASSWORD}" ) || \
@@ -11,65 +12,6 @@ if [[ ( -z "${POSTGRESQL_MAJOR_VERSION}" ) || \
     echo "Error! Some of environment variables are not set!" >&2
     exit 1
 fi
-
-sudo -u "${DATABASE_ADMIN_USER}" \
-env \
-POSTGRESQL_MAJOR_VERSION="${POSTGRESQL_MAJOR_VERSION}" \
-DATABASE_ADMIN_USER="${DATABASE_ADMIN_USER}" \
-DATABASE_ADMIN_GROUP="${DATABASE_ADMIN_GROUP}" \
-DATABASE_ADMIN_KEY="${DATABASE_ADMIN_KEY}" \
-DATABASE_ADMIN_KEY_PASSWORD="${DATABASE_ADMIN_KEY_PASSWORD}" \
-DATABASE_ADMIN_CSR="${DATABASE_ADMIN_CSR}" \
-bash << "EOF"
-
-echo "Running as $(whoami)."
-DATABASE_ADMIN_HOME="${HOME}"
-DATABASE_DATA_DIRECTORY=\
-"${DATABASE_ADMIN_HOME}/${POSTGRESQL_MAJOR_VERSION}/main"
-echo "Changing directory to ${DATABASE_DATA_DIRECTORY}"
-cd "${DATABASE_DATA_DIRECTORY}"
-
-if [[ ! -f "${DATABASE_ADMIN_KEY}" ]]; then
-    echo "Generating ${DATABASE_DATA_DIRECTORY}/${DATABASE_ADMIN_KEY}" \
-         "with ${DATABASE_ADMIN_USER}:${DATABASE_ADMIN_GROUP} ownership."
-    openssl genpkey \
-    -algorithm RSA \
-    -AES-256-CBC \
-    -pass env:DATABASE_ADMIN_KEY_PASSWORD \
-    -out "${DATABASE_ADMIN_KEY}" \
-    -quiet
-else
-    echo "${DATABASE_DATA_DIRECTORY}/${DATABASE_ADMIN_KEY}" \
-         "already exists, skipping."
-fi
-
-if [[ ! -f "${DATABASE_ADMIN_CSR}" ]]; then
-    echo "Generating ${DATABASE_DATA_DIRECTORY}/${DATABASE_ADMIN_CSR}" \
-         "with ${DATABASE_ADMIN_USER}:${DATABASE_ADMIN_GROUP} ownership."
-    # CN must match client's database role for ssl full verification.
-    openssl req \
-    -new \
-    -key "${DATABASE_ADMIN_KEY}" \
-    -passin env:DATABASE_ADMIN_KEY_PASSWORD \
-    -subj "/O=my_dear_doctor/OU=database/CN=${DATABASE_ADMIN_USER}" \
-    -out "${DATABASE_ADMIN_CSR}"
-else
-    echo "${DATABASE_DATA_DIRECTORY}/${DATABASE_ADMIN_CSR}" \
-         "already exists, skipping."
-fi
-
-echo "Finished running as $(whoami)."
-
-EOF
-
-sudo \
-env \
-POSTGRESQL_MAJOR_VERSION="${POSTGRESQL_MAJOR_VERSION}" \
-DATABASE_ADMIN_USER="${DATABASE_ADMIN_USER}" \
-DATABASE_ADMIN_GROUP="${DATABASE_ADMIN_GROUP}" \
-DATABASE_ADMIN_KEY="${DATABASE_ADMIN_KEY}" \
-DATABASE_ADMIN_CSR="${DATABASE_ADMIN_CSR}" \
-bash << "EOF"
 
 check_ownership()
 {
@@ -91,36 +33,6 @@ check_ownership()
     fi
 }
 
-echo "Running as $(whoami)."
-DATABASE_ADMIN_HOME=$(getent passwd "${DATABASE_ADMIN_USER}" | cut -d : -f 6)
-DATABASE_DATA_DIRECTORY=\
-"${DATABASE_ADMIN_HOME}/${POSTGRESQL_MAJOR_VERSION}/main"
-echo "Changing directory to ${DATABASE_DATA_DIRECTORY}"
-cd "${DATABASE_DATA_DIRECTORY}"
-
-check_ownership \
-"${DATABASE_DATA_DIRECTORY}/${DATABASE_ADMIN_KEY}" \
-"${DATABASE_ADMIN_USER}" \
-"${DATABASE_ADMIN_GROUP}"
-
-check_ownership \
-"${DATABASE_DATA_DIRECTORY}/${DATABASE_ADMIN_CSR}" \
-"${DATABASE_ADMIN_USER}" \
-"${DATABASE_ADMIN_GROUP}"
-
-echo "Finished running as $(whoami)."
-
-EOF
-
-sudo -u "${DATABASE_ADMIN_USER}" \
-env \
-POSTGRESQL_MAJOR_VERSION="${POSTGRESQL_MAJOR_VERSION}" \
-DATABASE_ADMIN_KEY="${DATABASE_ADMIN_KEY}" \
-DATABASE_ADMIN_KEY_PERMISSIONS="${DATABASE_ADMIN_KEY_PERMISSIONS}" \
-DATABASE_ADMIN_CSR="${DATABASE_ADMIN_CSR}" \
-DATABASE_ADMIN_CSR_PERMISSIONS="${DATABASE_ADMIN_CSR_PERMISSIONS}" \
-bash << "EOF"
-
 check_permissions()
 {
     local TARGET="$1"
@@ -138,26 +50,55 @@ check_permissions()
     fi
 }
 
-echo "Running as $(whoami)."
-DATABASE_ADMIN_HOME="${HOME}"
-DATABASE_DATA_DIRECTORY=\
-"${DATABASE_ADMIN_HOME}/${POSTGRESQL_MAJOR_VERSION}/main"
-echo "Changing directory to ${DATABASE_DATA_DIRECTORY}"
-cd "${DATABASE_DATA_DIRECTORY}"
+echo "Changing directory to ${DATABASE_CERTS_DIRECTORY}"
+cd "${DATABASE_CERTS_DIRECTORY}"
+
+if [[ ! -f "${DATABASE_ADMIN_KEY}" ]]; then
+    echo "Generating ${DATABASE_CERTS_DIRECTORY}/${DATABASE_ADMIN_KEY}"
+    openssl genpkey \
+    -algorithm RSA \
+    -AES-256-CBC \
+    -pass env:DATABASE_ADMIN_KEY_PASSWORD \
+    -out "${DATABASE_ADMIN_KEY}" \
+    -quiet
+else
+    echo "${DATABASE_CERTS_DIRECTORY}/${DATABASE_ADMIN_KEY}" \
+         "already exists, skipping."
+fi
+
+if [[ ! -f "${DATABASE_ADMIN_CSR}" ]]; then
+    echo "Generating ${DATABASE_CERTS_DIRECTORY}/${DATABASE_ADMIN_CSR}"
+    # CN must match client's database role for ssl full verification.
+    openssl req \
+    -new \
+    -key "${DATABASE_ADMIN_KEY}" \
+    -passin env:DATABASE_ADMIN_KEY_PASSWORD \
+    -subj "/O=my_dear_doctor/OU=database/CN=${DATABASE_ADMIN_USER}" \
+    -out "${DATABASE_ADMIN_CSR}"
+else
+    echo "${DATABASE_CERTS_DIRECTORY}/${DATABASE_ADMIN_CSR}" \
+         "already exists, skipping."
+fi
+
+check_ownership \
+"${DATABASE_CERTS_DIRECTORY}/${DATABASE_ADMIN_KEY}" \
+"${DATABASE_ADMIN_UID}" \
+"${DATABASE_ADMIN_GID}"
+
+check_ownership \
+"${DATABASE_CERTS_DIRECTORY}/${DATABASE_ADMIN_CSR}" \
+"${DATABASE_ADMIN_UID}" \
+"${DATABASE_ADMIN_GID}"
 
 check_permissions \
-"${DATABASE_DATA_DIRECTORY}/${DATABASE_ADMIN_KEY}" \
+"${DATABASE_CERTS_DIRECTORY}/${DATABASE_ADMIN_KEY}" \
 "${DATABASE_ADMIN_KEY_PERMISSIONS}"
 
 check_permissions \
-"${DATABASE_DATA_DIRECTORY}/${DATABASE_ADMIN_CSR}" \
+"${DATABASE_CERTS_DIRECTORY}/${DATABASE_ADMIN_CSR}" \
 "${DATABASE_ADMIN_CSR_PERMISSIONS}"
 
 ls -l "${DATABASE_ADMIN_KEY}"
 ls -l "${DATABASE_ADMIN_CSR}"
-
-echo "Finished running as $(whoami)."
-
-EOF
 
 echo
